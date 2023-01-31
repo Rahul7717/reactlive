75e7817b0d332a4df2eb35382d9c38a657e8ce16
rahul.dobariya <dobriya537@gmail.com>
2023-01-31 17:30:45 +0530

    Updates

---
+++ precache-manifest.84566b7e8728828dec8b93c05260befd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbdb217b6a3102fa9ec0",
+    "url": "/reactlive/static/css/main.d6c38059.chunk.css"
+  },
+  {
+    "revision": "cbdb217b6a3102fa9ec0",
+    "url": "/reactlive/static/js/main.cbdb217b.chunk.js"
+  },
+  {
+    "revision": "e459cc6ec89340cad24c",
+    "url": "/reactlive/static/js/1.e459cc6e.chunk.js"
+  },
+  {
+    "revision": "145084de1ecf457c2405",
+    "url": "/reactlive/static/js/2.145084de.chunk.js"
+  },
+  {
+    "revision": "7dc9af75cb0cdf448a15",
+    "url": "/reactlive/static/js/runtime~main.7dc9af75.js"
+  },
+  {
+    "revision": "95bf91025a317a046850158ea41f2161",
+    "url": "/reactlive/index.html"
+  }
+];
